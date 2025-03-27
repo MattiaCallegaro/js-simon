66dda714f34b41380c46c82900bc26684e82026d
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-27 14:20:37 +0100

    [UPD] Add DOM in js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//dichiarazione DOM
+const countdownElement = document.getElementById("countdown");
+const instructionsElement = document.getElementById("instructions");
+const numbersListElement = document.getElementById("numbers-list");
+const answersFormElement = document.getElementById("answers-form");
+const inputGroup = document.getElementById("input-group");
+const messageElement = document.getElementById("message");
+
