3c847d17686a785e01157ae11eeb600e10ba47fe
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-27 15:31:32 +0100

    [UPD] Add memorization numbers

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
 //dichiarazione DOM
+
 const countdownElement = document.getElementById("countdown");
 const instructionsElement = document.getElementById("instructions");
 const numbersListElement = document.getElementById("numbers-list");
 const answersFormElement = document.getElementById("answers-form");
-const inputGroup = document.getElementById("input-group");
+const inputElements = document.querySelectorAll("#input-group input");
 const messageElement = document.getElementById("message");
 
+//variabili di gioco
+
+let numeriDaRicordare= [];
+let timer;
+const tempoPerRicordare= 30;
+
+//genera 5 numeri casuali
+
+function numeriCasuali(){
+    const numeri = [];
+    while(numeri.length<5){
+        const num = Math.floor(Math.random()* 100)+1;
+        if(!numeri.includes(num)){
+            numeri.push(num);
+        } 
+    }
+    return numeri;
+   
+}
+const numeriGenerati = numeriCasuali();
+console.log("Numeri generati:", numeriGenerati);
+
+//Mostra i numeri da memorizzare
+
+function displayNumber(numbers){
+    numbersListElement.innerHTML = "";
+    numbers.forEach(num => {
+        const li = document.createElement("li");
+        li.textContent = num;
+        numbersListElement.appendChild(li);
+    });
+}
+displayNumber(numeriGenerati);
